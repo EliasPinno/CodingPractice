ef75522d4473b6439b91c626398e0ade62692eef
EliasPinno <pucas946@gmail.com>
2023-02-02 17:21:02 -0800

    Added better solution for the smallest solution.

--- LeetCode/230-Kth-Smallest.py
+++ LeetCode/230-Kth-Smallest.py
@@ -6,16 +6,14 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        items = deque()
-        self.traverse(root,items)
-        number = -1
-        for i in range(k):
-            number = items.popleft()
-            print(number)
-        return number.val
+        nodes = deque()
+        self.traverse(root, k, nodes)
+        return nodes.pop().val
 
-    def traverse(self, node: Optional[TreeNode], items: deque):
+    def traverse(self, node: Optional[TreeNode], k: int, nodes: deque) -> None:
         if node != None:
-            self.traverse(node.left,items)
-            items.append(node)
-            self.traverse(node.right,items)
+            self.traverse(node.left,k,nodes)
+            if len(nodes) < k:
+                nodes.append(node)
+                self.traverse(node.right,k,nodes)
+# O(k) space, time
