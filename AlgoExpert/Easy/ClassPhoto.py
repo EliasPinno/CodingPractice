07b0f3d106fe6a3bd8b55c7cbaeb6f4c593ef163
EliasPinno <pucas946@gmail.com>
2023-09-13 10:46:07 -0600

    Completed claas Photo problem.py

---
+++ AlgoExpert/Easy/ClassPhoto.py
@@ -0,0 +1,14 @@
+def classPhotos(redShirtHeights, blueShirtHeights):
+    redShirtHeights.sort(reverse=True)
+    blueShirtHeights.sort(reverse=True)
+    print(redShirtHeights)
+    print(blueShirtHeights)
+    if redShirtHeights[0] > blueShirtHeights[0]:
+        return verifyRow1FitsBehindRow2(redShirtHeights, blueShirtHeights)
+    return verifyRow1FitsBehindRow2(blueShirtHeights, redShirtHeights)
+
+def verifyRow1FitsBehindRow2(row1, row2):
+    for i in range(len(row1)):
+        if row2[i] >= row1[i]:
+            return False
+    return True
