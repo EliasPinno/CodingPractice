fe6a3b697b69e1731fde46e852274cdfba112adb
EliasPinno <pucas946@gmail.com>
2023-09-18 20:18:58 -0600

    Fixed a spelling issue.

--- AlgoExpert/Easy/CeaserCipher.py
+++ AlgoExpert/Easy/CeaserCipher.py
@@ -1,10 +1,10 @@
 def caesarCipherEncryptor(string, key):
     # Make the string a list for easier modification
     stringList = list(string)
-    solutionList = list(map(lambda c: ceaserCipherSingleChar(c,key), stringList))
+    solutionList = list(map(lambda c: caesarCipherSingleChar(c,key), stringList))
     return ''.join(solutionList)
 
-def ceaserCipherSingleChar(char, key):
+def caesarCipherSingleChar(char, key):
     charNumber = ord(char)
     alphabetValue = charNumber - 97
     alphabetValue = (alphabetValue + key) % 26
