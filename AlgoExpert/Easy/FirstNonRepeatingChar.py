2be5411251f4b27264e1d986bcc8b2ab9068a4ac
EliasPinno <pucas946@gmail.com>
2023-09-18 22:55:16 -0600

    Added missing changes.

--- AlgoExpert/Easy/FirstNonRepeatingChar.py
+++ AlgoExpert/Easy/FirstNonRepeatingChar.py
@@ -1,2 +1,16 @@
-for i in range(len("abc"),-1,-1):
-    print(i)
+def firstNonRepeatingCharacter(string):
+    characterMap = {}
+    for i in range(len(string)):
+        char = string[i]
+        characterMap[char] = i
+    for i in range(len(string)):
+        char = string[i]
+        if char in characterMap:
+            if characterMap[char] != i:
+                del characterMap[char]
+            else:
+                return i
+    return -1
+# O(n) time (two passes) through the list
+# O(1) space because there are only 26 characters in the alphabet for our input string
+# However, it is more realistically O(k), where k is the size of the input "alphabet"
