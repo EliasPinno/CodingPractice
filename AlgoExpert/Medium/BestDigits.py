ac38ff9ff3b0bcaa95e4adfdcf65ea0b5c2d6095
EliasPinno <pucas946@gmail.com>
2023-09-29 17:53:45 -0600

    Completed the BestDigits problem.

---
+++ AlgoExpert/Medium/BestDigits.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def bestDigits(number, numDigits):
+    stack = deque()
+    stack.append(number[0])
+    for i in range(1,len(number)):
+        print(list(stack))
+        #print(numDigits)
+        digit = number[i]
+        topDigit = stack[-1]
+        while digit > topDigit and len(stack) > 0 and numDigits > 0:
+            numDigits -= 1
+            topDigit = stack.pop()
+            if topDigit >= digit: # Check before we break the loop
+                stack.append(topDigit)
+                numDigits += 1
+        stack.append(digit)
+    for i in range(numDigits):
+        stack.pop()
+    # Write your code here.
+    return "".join(list(stack))
