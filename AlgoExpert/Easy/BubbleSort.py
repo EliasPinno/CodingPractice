e6eeda5ed99e8f1ceb700892a537553dd2063ef7
EliasPinno <pucas946@gmail.com>
2023-10-02 09:55:22 -0600

    Completed easy bubblesort.

---
+++ AlgoExpert/Easy/BubbleSort.py
@@ -0,0 +1,8 @@
+def bubbleSort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+# O(1) additional space, O(n^2) time
